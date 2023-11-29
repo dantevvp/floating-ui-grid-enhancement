be92b71208c17db1e23ed8a7a883b90b4c690720
Dante Ventieri <dantevvp@gmail.com>
2023-11-29 14:36:35 -0300

    fix missing deps

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { Box, CssBaseline } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import Header from '/components/Header';
-import { NavigableContainer } from '/utils/navigation';
-import { Navigable } from '/components/navigation/Navigable';
 
 export default function Layout(props) {
   return (
-    <div className="container mx-auto my-[200px] h-screen">
+    <div className="container mx-auto my-[200px] h-full">
       {props.children}
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import { H1 } from '../components/Typography'
 export default function Home() {
   return (
     <div className="container mx-auto my-auto">
-      <div className="grid place-items-center gap-4 h-screen">
+      <div className="grid place-items-center gap-4">
         <div className="grid place-items-center">
           <H1 className="mb-4">Navegando con el teclado</H1>
           <Link href="test/1"><Button>Ver demo</Button></Link>
